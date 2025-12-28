d5d7db35ff89d8d4470513f2fc42f7c31ea9017e
Dimitrov <dimitrov@users.noreply.github.com>
2025-12-28 16:23:47 +0800

    push

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-// OPEN INVITATION
 function openInvite(){
   document.getElementById("opening").style.display = "none";
   document.getElementById("main").style.display = "block";
   document.getElementById("music").play();
 }
 
-// NAMA TAMU DARI URL
+// Nama tamu dari URL
 const params = new URLSearchParams(window.location.search);
 const guest = params.get("to");
 if(guest){
-  document.getElementById("guest").innerText = guest;
+  document.getElementById("guest").innerText = guest.replace(/\+/g," ");
 }
 
-// COUNTDOWN
+// Countdown
 const targetDate = new Date("2026-01-12T09:00:00").getTime();
 const countdown = document.getElementById("countdown");
 
 setInterval(()=>{
   const now = new Date().getTime();
   const diff = targetDate - now;
+
+  if(diff <= 0){
+    countdown.innerHTML = "Hari Bahagia Telah Tiba 💍";
+    return;
+  }
 
   const d = Math.floor(diff / (1000*60*60*24));
   const h = Math.floor((diff / (1000*60*60)) % 24);
